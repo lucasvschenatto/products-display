fa31c2276a7636134d2b615f97e653a48bf9b31e
Lucas Schenatto <lucasvschenatto@hotmail.com>
2023-02-22 14:00:58 -0300

    add products and theme types

---
+++ src/types/product.d.ts
@@ -0,0 +1,13 @@
+export type Product = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: rating;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+};
