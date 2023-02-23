e8a953a489ad2aaee375a505e4ffcb2903e91789
Lucas Schenatto <lucasvschenatto@hotmail.com>
2023-02-23 01:33:04 -0300

    refactor to remove duplicated code

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,10 +1,9 @@
 import { Product } from "@/types/product";
 import axios, { AxiosError } from "axios";
 
-export async function getProducts(): Promise<Product[]> {
-  const productAPI = `${process.env.HOST ?? ""}/api/products`;
+async function getData<T>(url: string): Promise<T> {
   try {
-    const { data } = await axios.get(productAPI);
+    const { data } = await axios.get<T>(url);
     return data;
   } catch (error) {
     const axiosError = error as AxiosError;
@@ -20,22 +19,12 @@ export async function getProducts(): Promise<Product[]> {
   }
 }
 
+export async function getProducts(): Promise<Product[]> {
+  const productsAPI = `${process.env.HOST ?? ""}/api/products`;
+  return getData(productsAPI);
+}
+
 export async function getProduct(id: string): Promise<Product> {
   const productAPI = `${process.env.HOST ?? ""}/api/products/${id}`;
-  try {
-    const { data } = await axios.get(productAPI);
-    return data;
-  } catch (error) {
-    console.log(error);
-    const axiosError = error as AxiosError;
-    if (axiosError.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      throw {
-        message: axiosError.message,
-        code: parseInt(axiosError.code ?? "500"),
-      };
-    }
-    throw { message: "Something wrong happened", code: 500 };
-  }
+  return getData(productAPI);
 }
