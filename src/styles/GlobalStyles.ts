b320d80e2462a7315219d7be3822843d3ce9e9fa
Lucas Schenatto <lucasvschenatto@hotmail.com>
2023-02-23 00:00:07 -0300

    enhance global styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -8,25 +8,21 @@ const GlobalStyle = createGlobalStyle<{ theme: DefaultTheme }>`
 //========================================================================================================
 // GENERAL
 //========================================================================================================
+:root{
+	--logo-color: ${({ theme }) => theme.colors.white}
+}
+
 * {
 	box-sizing: border-box;
 	color: ${({ theme }) => theme.colors.white};
 }
+
 *::before {
 	box-sizing: border-box;
 }
+
 *::after {
 	box-sizing: border-box;
-}
-
-span,label,p{
-	font-family: ${lato400.style.fontFamily};
-	font-weight: ${lato400.style.fontWeight};
-}
-
-h3,h4,button{
-	font-family: ${cinzel.style.fontFamily};
-	font-weight: ${cinzel.style.fontWeight};
 }
 
 body {
@@ -34,6 +30,44 @@ body {
 	margin:unset;
 }
 
+main{
+	margin-top: ${({ theme }) => theme.spacing["2xl"]};
+}
+
+span,p{
+	font-family: ${lato400.style.fontFamily};
+	font-weight: ${lato400.style.fontWeight};
+	text-transform: capitalize;
+	margin:0;
+}
+
+em{
+	color: ${({ theme }) => theme.colors.gold};
+}
+
+h1,h2,h3,h4,button{
+	font-family: ${cinzel.style.fontFamily};
+	font-weight: ${cinzel.style.fontWeight};
+	margin: 0 auto;
+	overflow: hidden;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+}
+
+h2,h3,h4,button{
+	text-align: center;
+}
+
+h3,h4{
+	font-size: ${({ theme }) => theme.fontSize.b};
+}
+
+a{
+	text-decoration: none;
+	* {
+		cursor: pointer;
+	}
+}
 `;
 
 export default GlobalStyle;
